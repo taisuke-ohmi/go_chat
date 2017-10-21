42ba816d9206062a948e5e5dfb14315d1fbd4a98
taisuke-ohmi <taisukeohmi0420@gmail.com>
2017-10-21 10:53:01 +0900

    include chat and trace directory

---
+++ chat/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	// r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルームを開始します
+	go r.run()
+	// Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
